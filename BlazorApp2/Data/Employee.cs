a60a7ced5fdffdd44bbeab4e7a340843e9604184
Hlogi Bale <Hlogi Bale@L-BALE>
2022-10-17 16:32:51 +0200

    Add project files.

---
+++ BlazorApp2/Data/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorApp2.Data
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Contact { get; set; }
+        public string Address { get; set; }
+
+    }
+}
---
+++ BlazorApp2/Data/EmployeeService.cs
@@ -0,0 +1,47 @@
+﻿namespace BlazorApp2.Data
+{
+    public class EmployeeService : IEmployeeService
+    {
+
+        private List<Employee> employees = new List<Employee>
+        {
+            new Employee { Id = Guid.NewGuid(),
+            Name= "Emplle",
+            Contact="0978484",
+            Address="Santon"},
+            new Employee { Id = Guid.NewGuid(),
+            Name= "Employee ",
+            Contact="0978484",
+            Address="Sandton"}
+        };
+
+        public void AddEmployee(Employee employee)
+        {
+            var id = Guid.NewGuid();
+            employee.Id= id;
+            employees.Add(employee);
+        }
+
+        public void DeleteEmployee(Guid id)
+        {
+            var employee= GetEmployee(id);
+            employees.Remove(employee);
+        }
+
+        public Employee GetEmployee(Guid id)
+        {
+            return employees.SingleOrDefault(x => x.Id == id);
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            return employees;
+        }
+
+        public void UpdateEmployee(Employee employee)
+        {
+            var getOldEmployee = GetEmployee(employee.Id);
+            getOldEmployee.Name = employee.Name;
+        }
+    }
+}
